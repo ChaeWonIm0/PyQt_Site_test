4039d7b15a21ad004d0a7d238945f259fb64e2c7
dla9944 <dla9944@naver.com>
2023-07-28 11:37:56 +0900

    README edit

---
+++ QFontdialog.py
@@ -0,0 +1,40 @@
+
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QColor
+
+class MyApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        btn = QPushButton('Dialog', self)
+        btn.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        btn.move(20, 20)
+        btn.clicked.connect(self.showDialog)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(btn)
+
+        self.lbl = QLabel("test", self)
+        self.lbl.move(130, 20)
+
+        vbox.addWidget(self.lbl)
+        self.setLayout(vbox)
+
+        self.setWindowTitle('Font DIalog')
+        self.setGeometry(300, 200, 250, 250)
+        self.show()
+
+    def showDialog(self):
+        font, ok = QFontDialog.getFont()
+
+        if ok:
+            self.lbl.setFont(font)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
--- Readme.md
+++ Readme.md
@@ -7,4 +7,15 @@
 
 ### 사용한 언어 : Python3
 
-- tool : Visual studio 2022, Pyqt5, 
+- tool : Visual studio 2022, Pyqt5, C++
+'''
+Qt is set of cross-platform C++ libraries that implement high-level APIs for
+accessing many aspects of modern desktop and mobile systems.  These include
+location and positioning services, multimedia, NFC and Bluetooth connectivity,
+a Chromium based web browser, as well as traditional UI development.
+
+PyQt5 is a comprehensive set of Python bindings for Qt v5.  It is implemented
+as more than 35 extension modules and enables Python to be used as an
+alternative application development language to C++ on all supported platforms
+including iOS and Android.
+'''
