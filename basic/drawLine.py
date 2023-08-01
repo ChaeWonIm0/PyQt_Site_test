3f98af7f0c30a439431682b9a396d21a320dc6d6
dla9944 <dla9944@naver.com>
2023-08-01 14:58:00 +0900

    line YEE

--- basic/drawLine.py
+++ basic/drawLine.py
@@ -21,14 +21,30 @@ class MyApp(QWidget):
 		qp.end()
 
 	def draw_line(self, qp):
+		qp.setPen(QPen(Qt.blue, 7))
+		qp.drawLine(104, 156, 134, 216)
 		qp.setPen(QPen(Qt.blue, 8))
-		qp.drawLine(30, 230, 200, 50)
+		qp.drawLine(173, 142, 134, 216)
+		qp.setPen(QPen(Qt.blue, 8))
+		qp.drawLine(150, 348, 134, 216)
 
-		qp.setPen(QPen(Qt.green, 8))
-		qp.drawLine(100, 230, 270, 50)
+		qp.setPen(QPen(Qt.yellow, 7))
+		qp.drawLine(233, 163, 322, 163)
+		qp.setPen(QPen(Qt.yellow, 7))
+		qp.drawLine(233, 163, 233, 331)
+		qp.setPen(QPen(Qt.yellow, 7))
+		qp.drawLine(233, 238, 322, 238)
+		qp.setPen(QPen(Qt.yellow, 7))
+		qp.drawLine(233, 326, 329, 326)
 
-		qp.setPen(QPen(Qt.red, 8))
-		qp.drawLine(170, 230, 340, 50)
+		qp.setPen(QPen(Qt.red, 7))
+		qp.drawLine(413, 163, 502, 163)
+		qp.setPen(QPen(Qt.red, 7))
+		qp.drawLine(413, 163, 413, 331)
+		qp.setPen(QPen(Qt.red, 7))
+		qp.drawLine(413, 238, 502, 238)
+		qp.setPen(QPen(Qt.red, 7))
+		qp.drawLine(413, 326, 509, 326)
 
 
 
